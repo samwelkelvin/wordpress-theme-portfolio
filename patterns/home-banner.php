5329d8ab17aa5130664d04a9e705d4da16817fea
SAM <samwelmunyaka@gmail.com>
2024-12-05 14:04:49 +0300

    Added patterns

---
+++ patterns/home-banner.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Title: Home page banner
+ * Slug: portfolio/home-banner
+ * Categories: banner
+ * Description: A large full cover banner with call to action button.
+ */
+
+?>
+<section id="hero" class="hero section">
+
+    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+
+    <div class="container text-center" data-aos="zoom-out" data-aos-delay="100">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h2>Freelancer</h2>
+                <p>I'm a professional wordpress developer</p>
+                <a href="about" class="btn-get-started">About Me</a>
+            </div>
+        </div>
+    </div>
+
+</section
