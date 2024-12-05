fa2b612124ad3aacdc1e2d4e763e5156d984467b
SAM <samwelmunyaka@gmail.com>
2024-12-05 13:17:51 +0300

    Updated footer.php

--- footer.php
+++ footer.php
@@ -11,15 +11,19 @@
             <a href=""><i class="bi bi-linkedin"></i></a>
         </div>
         <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you've purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
     </div>
 
 </footer>
+
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+</a>
+
+<!-- Preloader -->
+<div id="preloader"></div>
 
 <?php
 wp_footer();
