0852a5c4705bccf4135b8b19330fb7aec6b17289
SAM <samwelmunyaka@gmail.com>
2024-12-05 13:25:03 +0300

    Upadted

--- page.php
+++ page.php
@@ -4,53 +4,21 @@
  * Loads static page like contact,homepage,landing page etc.
  */
 get_header();
-
-if (is_front_page()) {
-    echo '<div class="container">';
-
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-
-            the_content();
-        }
-    } else {
-        echo '<h3>No Page data </h3>';
-    }
-    echo '</div>';
-
-    // get_template_part('template-parts/content', 'front-page');
-} elseif (is_home()) {
-    echo '<div class="container">';
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-
-            the_content();
-        }
-    } else {
-        echo '<h3>No Page data </h3>';
-    }
-    echo '</div>';
-
-    // get_template_part('template-parts/content', 'front-page');
-} else {
-    // get_template_part('template-parts/content', 'page');
-    echo '<div class="container">';
-
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-
-            the_content();
-        }
-    } else {
-        echo '<h3>No Page data </h3>';
-    }
-    echo '</div>';
-}
-
 ?>
+<main class="main"> 
+    <?php
 
- <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+
+                the_content();
+            }
+        } else {
+            echo '<h3>No Page data </h3>';
+        }
+
+    ?>
+</main>
+<?php
     get_footer();
